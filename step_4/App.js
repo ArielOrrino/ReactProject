a1fe55d3733de4b81bd15dd77e62339c7056222b
alan <alan.brunoab@gmail.com>
2018-11-01 17:50:01 -0300

    Pantalla Home, Botón a pantalla Contadores

--- step_4/App.js
+++ step_4/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Button } from 'react-native';
 import Controls from './components/Controls';
 import CounterContainer from './components/CounterContainer';
 import store from './state/store';
+import {  createStackNavigator, StackActions, NavigationActions} from 'react-navigation';
+
 
 class App extends React.Component {
   render() {
     return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
+          <Text>Totales</Text>
+          <Button style={{}}
+            title="Agregar"
+            onPress={() => {
+              this.props.navigation.dispatch(StackActions.reset({
+                index: 0,
+                actions: [
+                  NavigationActions.navigate({ routeName: 'Details' })
+                ],
+              }))
+            }}
+          />
+        </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      /*<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>*/
       <ReduxProvider store={store}>
         <View style={styles.container}>
           <Text style={styles.title}>
             Contadores de cosas que rompió patu!
           </Text>
-
           <Controls />
-
           <ScrollView style={styles.scrollViewContainer}>
             <CounterContainer />
           </ScrollView>
@@ -24,6 +48,26 @@ class App extends React.Component {
     );
   }
 }
+
+export default createStackNavigator({
+  Home: {
+    screen: App,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+}, {
+    initialRouteName: 'Home',
+});
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -45,4 +89,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+
+
+//export default App;
